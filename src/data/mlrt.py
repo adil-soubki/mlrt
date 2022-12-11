c50886781f61356de9651f847df4144bb1c71e16
Adil Soubki <asoubki@login2.cm.cluster>
2022-12-11 13:00:08 -0500

    update pylintrc, fix pre-commit hook, add utils for working with MLRegTest data

--- src/data/mlrt.py
+++ src/data/mlrt.py
@@ -2,10 +2,10 @@
 import functools
 import glob
 import os
-from dataclasses import dataclass 
+from dataclasses import dataclass
 from typing import Any
 
-import pandas as pd
+import pandas as pd  # type: ignore
 
 
 FF_DIR = os.path.abspath("/gpfs/projects/HeinzGroup/asoubki/FlexFringe/")
@@ -26,15 +26,15 @@ def validate_alphabet(alphabet_size: int) -> list[str]:
 @dataclass
 class MLRegTestFile:
     path: str
-    data_size: str      # Small, Mid, Large
-    split: str          # Train, Dev, Test(SR/LR/SA/LA)
+    data_size: str  # Small, Mid, Large
+    split: str  # Train, Dev, Test(SR/LR/SA/LA)
     alphabet_size: int  # 4, 16, 64
     tier_size: int
     language_class: str
     factor_width: int
     threshold: int
     index: int
-    
+
     @classmethod
     def from_path(cls, path: str) -> MLRegTestFile:
         bname = os.path.basename(path)
@@ -48,9 +48,9 @@ class MLRegTestFile:
             language_class=mdata[2],
             factor_width=int(mdata[3]),
             threshold=int(mdata[4]),
-            index=int(mdata[5])
+            index=int(mdata[5]),
         )
-    
+
     def to_string(self) -> str:
         lines = []
         header, samples = self.to_flexfringe()
@@ -58,7 +58,7 @@ class MLRegTestFile:
         for smpl in samples:
             lines.append(" ".join(map(str, [smpl[0], smpl[1]] + smpl[2])))
         return "\n".join(lines)
-    
+
     def to_flexfringe(self) -> tuple[list[Any], list[Any]]:
         # TODO: validate small, mid, large have 1k, 10k, and 100k samples.
         alphabet = validate_alphabet(64)
@@ -70,6 +70,6 @@ class MLRegTestFile:
             assert len(smpl) == len(dct["sample"])
             samples.append((int(dct["label"]), len(smpl), smpl))
         return header, samples
-            
+
     def to_df(self) -> pd.DataFrame:
         return pd.read_csv(self.path, sep="\t", names=["sample", "label"])
