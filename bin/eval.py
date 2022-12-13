9cf8f54cc000d50189a05e76de533b4ca5e403ec
Adil Soubki <asoubki@login2.cm.cluster>
2022-12-13 15:23:05 -0500

    add code and utils for evaluating models against datasets.

---
+++ bin/eval.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""An example script"""
+import os
+from dataclasses import asdict
+from glob import glob
+from typing import Any
+
+import evaluate
+import pandas as pd
+from tqdm import tqdm
+
+from src.core.context import Context
+from src.core.app import harness
+from src.bin.flexfringe import FF_DIR
+from src.data.flexfringe import FFModel
+
+
+MLRT_DIR = os.path.join(FF_DIR, "data", "MLRegTest")
+
+
+def compute(model: FFModel, path: str) -> dict[str, Any]:
+    ret = {k: v for k, v in vars(model).items() if k not in ("path", "machine", "dfa")}
+    ret["split"] = os.path.splitext(os.path.basename(path))[0].split("_")[-1]
+    df = pd.DataFrame(map(asdict, model.evaluate(path)))
+    for metric in ("precision", "recall", "f1", "brier_score"): #  "roc_auc"?
+        ret |= evaluate.load(metric).compute(predictions=df.pred, references=df.label)
+    return ret
+
+
+def main(ctx: Context) -> None:
+    ctx.parser.add_argument("paths", nargs="+", help="model paths")
+    ctx.parser.add_argument("-o", "--outpath", default=os.path.join(FF_DIR, "evals.csv"))
+    args = ctx.parser.parse_args()
+
+    results = []
+    for path in tqdm(args.paths):
+        assert path.endswith(".final.json")
+        _, dstr, msize = os.path.basename(path).replace(".final.json", "").split("_")
+        model = FFModel.from_path(path)
+        for dpath in tqdm(glob(os.path.join(MLRT_DIR, msize, f"{dstr}_Test*")), leave=False):
+            results.append(compute(model, dpath))
+    ctx.log.info("writing: %s", args.outpath)
+    pd.DataFrame(results).to_csv(args.outpath, index=False)
+
+
+if __name__ == "__main__":
+    harness(main)
---
+++ src/data/flexfringe.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*
+from __future__ import annotations
+
+import json
+import os
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Iterator
+
+
+@dataclass
+class FFData:
+    path: str
+    header: list[int]
+    samples: list[tuple[int, int, list[int]]]
+
+    @classmethod
+    def from_path(cls, path: str) -> FFData:
+        samples = []
+        with open(path, "r") as fd:
+            header = list(map(int, next(fd).strip().split()))
+            for ln in fd:
+                line = list(map(int, ln.strip().split()))
+                assert line[1] == len(line[2:])
+                samples.append((line[0], line[1], line[2:]))
+            assert header[0] == len(samples)
+            return cls(path=path, header=header, samples=samples)
+
+
+@dataclass
+class FFModelResult:
+    seq: list[int]
+    label: bool
+    pred: bool
+
+
+@dataclass
+class FFModel:
+    path: str
+    machine: dict[str, Any]
+    dfa: dict[str, dict[str, Any]]
+    ini: str  # E.g. esdm
+    data_size: str  # Small, Mid, Large
+    alphabet_size: int  # 4, 16, 64
+    tier_size: int
+    language_class: str
+    factor_width: int
+    threshold: int
+    index: int
+
+    @classmethod
+    def from_path(cls, path: str) -> FFModel:
+        with open(path, "r") as fd:
+            machine = json.load(fd)
+        # {src1: {inp1: tgt1, ..., is_final: True/False}, ...}
+        dfa: dict[str, dict[str, Any]] = defaultdict(dict)
+        # dfa[start_state][input] = end_state
+        for edge in machine["edges"]:
+            dfa[edge["source"]][edge["name"]] = edge["target"]
+        # dfa[state_id]["is_final"] = True/False
+        for node in machine["nodes"]:
+            dfa[str(node["id"])]["is_final"] = node["data"]["total_final"] > 0
+        # assign metadata
+        bname = os.path.basename(path)
+        mdata = bname.split("_")[1].split(".")
+        return cls(
+            path=path,
+            machine=machine,
+            dfa=dfa,
+            ini=bname.split("_")[0],
+            data_size=bname.split("_")[-1].split(".")[0],
+            alphabet_size=int(mdata[0]),
+            tier_size=int(mdata[1]),
+            language_class=mdata[2],
+            factor_width=int(mdata[3]),
+            threshold=int(mdata[4]),
+            index=int(mdata[5]),
+        )
+
+    def __call__(self, seq: list[int]) -> bool:
+        state = "0"
+        for sym in seq:
+            state = self.dfa[state].get(str(sym), None)
+            if state is None:
+                return False
+        assert isinstance(self.dfa[state]["is_final"], bool)
+        return bool(self.dfa[state]["is_final"])
+
+    def evaluate(self, path: str) -> Iterator[FFModelResult]:
+        for label, _, seq in FFData.from_path(path).samples:
+            assert label in (0, 1)
+            yield FFModelResult(seq, bool(label), self(seq))
